86d77f0e206f7ed613daa79b026db4075619693e
FlorianS4 <florian.sassmann@hotmail.com>
2025-03-30 16:27:45 +0200

    feat:customize product and category admin panel

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,27 @@ from django.contrib import admin
 from .models import GameProduct, Category
 
 # Register your models here.
-admin.site.register(GameProduct)
-admin.site.register(Category)
+
+
+class GameProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'product_name',
+        'category',
+        'price',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(GameProduct, GameProductAdmin)
+admin.site.register(Category, CategoryAdmin)
