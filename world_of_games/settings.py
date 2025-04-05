1ce6f128222a2938ef412d055624f32f878c23ad
FlorianS4 <florian.sassmann@hotmail.com>
2025-04-05 11:34:51 +0200

    feat: add add to shoppingbag button to product_detail template

--- world_of_games/settings.py
+++ world_of_games/settings.py
@@ -162,5 +162,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-FREE_DELIVERY_PERCENTAGE = 100
+FREE_DELIVERY_THRESHOLD = 100
 STANDARD_DELIVERY_PERCENTAGE = 10
