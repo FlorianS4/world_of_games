7d180f7407fd81b3289000b16a898ad654faa539
FlorianS4 <florian.sassmann@hotmail.com>
2025-04-05 14:08:46 +0200

    feat: add option to choose between digital or physical purchase to model and add selector box to template

--- products/models.py
+++ products/models.py
@@ -23,6 +23,7 @@ class GameProduct(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
     product_name = models.CharField(max_length=254)
     product_description = models.TextField()
+    physical_or_digital = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
