863f0dee8cf97885ac2c88eca590716b9baf7016
FlorianS4 <florian.sassmann@hotmail.com>
2025-04-12 11:34:45 +0200

    feat: Add order summary to checkout success page

--- checkout/views.py
+++ checkout/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.conf import settings
-from .forms import Order, OrderForm
-from .models import OrderLineItem
+
+from .forms import OrderForm
+from .models import Order, OrderLineItem
 from products.models import GameProduct
 from shoppingbag.contexts import shoppingbag_contents
 
