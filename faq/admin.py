e9fe4d08cf3c25cfa18d750f5038d290cb85697b
FlorianS4 <florian.sassmann@hotmail.com>
2025-04-20 15:46:56 +0200

    feat: Add bootstrap accordion for faq

--- faq/admin.py
+++ faq/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import FAQ
 
 # Register your models here.
+
+
+class FAQAdmin(admin.ModelAdmin):
+    """
+    Admin interface for managing FAQ content.
+    """
+
+    list_display = (
+        'question',
+        'answer'
+        )
+
+
+admin.site.register(FAQ, FAQAdmin)
